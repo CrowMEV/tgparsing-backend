b178bfc3130f7942983ee200efa0ae2d4f9a638d
lexa.shvetsoff <lexa.shvetsoff@yandex.ru>
2023-05-23 23:58:32 +0800

    add autorization

---
+++ api/api_main.py
@@ -0,0 +1,55 @@
+from typing import Optional, List
+
+from fastapi import FastAPI, Depends, Response, Query, File, UploadFile, status
+from fastapi_users import FastAPIUsers
+from fastapi.middleware.cors import CORSMiddleware
+
+from starlette.responses import FileResponse
+
+from auth.database import User
+from auth.manager import get_user_manager
+from auth.auth import auth_backend
+from auth.schemas import UserRead, UserCreate
+
+
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
+
+
+app = FastAPI(title='TgParsing')
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
+
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
+
+
+current_user = fastapi_users.current_user()
+
+
+@app.get("/protected-route")
+def protected_route(user: User = Depends(current_user)):
+    return f"Hello, {user.firstname}"
