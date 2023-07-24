6444f72a5e694dd98d57e9318114525695a44862
Evgeny Malikov <mev@offershub.ru>
2023-07-24 11:43:11 +0500

    alembic merge

---
+++ backend/migrations/versions/2023-07-24_11:42:39-245907d587fd_merge_add_tasks_add_user_phone.py
@@ -0,0 +1,24 @@
+"""merge add tasks add user phone
+
+Revision ID: 245907d587fd
+Revises: 8f82e9ab0796, 089845b2b7c2
+Create Date: 2023-07-24 11:42:39.030639
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '245907d587fd'
+down_revision = ('8f82e9ab0796', '089845b2b7c2')
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
