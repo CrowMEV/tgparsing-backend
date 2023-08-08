3cc08a031ef8cda01ca826ea34c3975c8f152556
Evgeny Malikov <mev@offershub.ru>
2023-08-09 02:51:06 +0500

    remove traefik auth middleware

--- app/run.sh
+++ app/run.sh
@@ -1,3 +1,3 @@
 #!bin/sh
 alembic upgrade head 
-gunicorn server:app --workers 5 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0
+gunicorn server:app --workers 5 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0
